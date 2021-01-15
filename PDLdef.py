86be768c7020bbcbc7e1e5ccaa043d19eafcf954
ekaekaekary <eryabova04@bk.ru>
2021-01-15 15:52:30 +0300

    .

--- PDLdef.py
+++ PDLdef.py
@@ -1,13 +1,28 @@
 from PIL import ImageColor, Image, ImageDraw
+from random import randint
+
 
 k = None
 b = None
+a = None
+c = None
+y_f = None
+y = None
+x_f = None
+x = None
+asc = True
+
+def color():
+    r = randint(1,254)
+    g = randint(1,254)
+    b = randint(1,254)
+    return r, g, b
 
 
+image = Image.new("RGB", (1200, 800))  # создание изображения
+draw = ImageDraw.Draw(image)
 # координатная плоскость
 def coordinate_plane():
-    image = Image.new("RGB", (1200, 800))  # создание изображения
-    draw = ImageDraw.Draw(image)
     draw.rectangle((0, 0, 1200, 800), fill=ImageColor.getrgb("white"))  # белый экран
     draw.line(((1200, 400, 1195, 405)), (0,0,0))
     draw.line(((600, 0, 600, 800)), (0,0,0))
@@ -16,45 +31,57 @@ def coordinate_plane():
     draw.line(((600, 0, 605, 5)), (0,0,0))
     draw.line(((1195, 395, 1200, 400)), (0,0,0))
     draw.line(((1200, 400, 1195, 405)), (0,0,0))
-    return image, draw
 
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+def convert(point):
+    global x, y
+    return Point(x, y)
 
 
 # линейная функция
-def line_asc():
-    print('y=k*x+b')
-    k = int(input("Введите k:  "))
-    b = int(input("Введите b:  "))
-    return k,b
-
 def line_f(x_f):
-    global k,b
-    y_f = k*x_f +b
+    global asc
+    if asc:
+        global k, b
+        print('y=k*x+b')
+        k = bool(input("Введите k:  "))
+        b = bool(input("Введите b:  "))
+        asc = False
+    y_f = bool(k*x_f+b)
     return y_f
 
 
 
 # квадратичная функция
-def quadratic_asc():
-    print('y=a*x^2+b*x+c')
-    a = int(input("Введите a:  "))
-    b = int(input("Введите b:  "))
-    c = int(input("Введите c:  "))
-    return a, b, с
-
-
 def quadratic_f(x_f):
+    global asc
+    if asc:
+        global a, b, c
+        print('y=a*x^2+b*x+c')
+        a = int(input("Введите a:  "))
+        b = int(input("Введите b:  "))
+        c = int(input("Введите c:  "))
+        asc = False
     y_f =a*x_f**2+b*x_f+c
-    return x_f, y_f
+    return y_f
 
 
 
 
 # обратная пропорциональность
-def gip_f(x):
-    print ('y=k/x')
-    k = int(input("Введите k:  "))
-    y =k/x
-    return x, y
+def gip_f(x_f):
+    global asc
+    if asc:
+        global k
+        print ('y=k/x')
+        k = int(input("Введите k:  "))
+        asc = False
+    y_f =k/x_f
+    return y_f
 
 
--- parametrically defined lines.py
+++ parametrically defined lines.py
@@ -1,30 +1,14 @@
-from PIL import Image, ImageColor, ImageDraw
-from random import randint
-from PDLdef import line_asc, line_f
+from PDLdef import coordinate_plane, convert, color
+from PDLdef import line_f, quadratic_f, gip_f
+from PDLdef import image, draw, k, b, a, c, asc, x, x_f, y, y_f, Point
 
-image = Image.new("RGB", (1200, 800))  # создание изображения
-draw = ImageDraw.Draw(image)
-draw.rectangle((0, 0, 1200, 800), fill=ImageColor.getrgb("white"))  # белый экран
-draw.line(((1200, 400, 1195, 405)), (0,0,0))
-draw.line(((600, 0, 600, 800)), (0,0,0))
-draw.line(((0, 400, 1200, 400)), (0,0,0))
-draw.line(((595, 5, 600, 0)), (0,0,0))
-draw.line(((600, 0, 605, 5)), (0,0,0))
-draw.line(((1195, 395, 1200, 400)), (0,0,0))
-draw.line(((1200, 400, 1195, 405)), (0,0,0))
+
+coordinate_plane
 
 
 game = True
 
-
-
 points =[]
-r = randint(1,254)
-g = randint(1,254)
-b = randint(1,254)
-
-k = None
-b = None
 
 start_x = 0
 end_x = 1200
@@ -34,20 +18,21 @@ x_f = -600
 x = start_x
 
 answer = input("(1)Линейная функция\n(2)Квадратичная функция\n(3)Обратная пропорциональность\n>>>\t")
-if answer == "1":
-    line_asc()
-    print(line_f(5))
-
-
 while x <= end_x:
     x+= 0.5
     x_f+= 0.5
-    if answer == 1:
-        line_f(x_f)
-        y = 400 - y_f
-        points.append(x,y)
-for point in points:
-    draw.point((point),(r,g,b))
+    if answer == "1":
+        print(line_f(x_f))
+    elif answer == "2":
+        print(quadratic_f(x_f))
+    elif answer == "3":
+        print(gip_f(x_f))
+
+    y = (not y_f) + 400
+    points.append(Point(x,y))
+    for point in points:
+        p = convert(point)
+        draw.point((p.x, p.y),color())
 
 
 
