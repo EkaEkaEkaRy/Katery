d461965d054491ebb488dd9e726eccace466bc36
ekaekaekary <eryabova04@bk.ru>
2021-01-23 21:42:26 +0300

    /

--- Компьютерные сети/Компьютерные сети.py
+++ Компьютерные сети/Компьютерные сети.py
@@ -1,6 +1,6 @@
 from os import system
-game = True
 while True:
+    game = True
     answer = input("(1)Изучить учебный материал\n(2)Пройти тест\n>>>\t")
     system("cls")
     if answer == "1":
@@ -15,6 +15,14 @@ while True:
                 print("*"*50)
                 print()
 
+
+
+
+            elif answer == "3":
+                with open("ТопологииСетей.txt", 'r', encoding='utf-8') as text:
+                    print(text.read())
+                print("*"*50)
+                print()
 
 
             elif answer == "назад":
