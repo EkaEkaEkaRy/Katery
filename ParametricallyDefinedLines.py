2de0b96019fdd00cc361b5394ab382b4148d824e
ekaekaekary <eryabova04@bk.ru>
2021-03-13 17:17:59 +0300

    Update ParametricallyDefinedLines.py

--- ParametricallyDefinedLines.py
+++ ParametricallyDefinedLines.py
@@ -1,16 +1,10 @@
 import pygame
 from random import randint
 pygame.init()
-gamedisplay = pygame.display.set_mode((800,600)) # экран размером 800 на 600
+gamedisplay = pygame.display.set_mode((1200,800)) # экран размером 800 на 600
 gamedisplay.fill((255,255,255)) # заливка экрана желтым
 # координатная плоскость
-gamedisplay.line(((1200, 400, 1195, 405)), (0, 0, 0))
-gamedisplay.line(((600, 0, 600, 800)), (0, 0, 0))
-gamedisplay.line(((0, 400, 1200, 400)), (0, 0, 0))
-gamedisplay.line(((595, 5, 600, 0)), (0, 0, 0))
-gamedisplay.line(((600, 0, 605, 5)), (0, 0, 0))
-gamedisplay.line(((1195, 395, 1200, 400)), (0, 0, 0))
-gamedisplay.line(((1200, 400, 1195, 405)), (0, 0, 0))
+N = 20
 
 r = 0
 g = 0
@@ -26,4 +20,23 @@ while game:
     for event in events: # берем события по одному
         if event.type == pygame.QUIT: # закрытие окна
             game = False
+    """
+    answer = input("(1)Линейная функция\n(2)Квадратичная функция\n(3)Обратная пропорциональность\n>>>\t")
 
+    r = randint(0, 255)
+    g = randint(0, 255)
+    b = randint(0, 255)
+
+    if answer == '1':
+        print('y=k*x+b')
+        k = float(input("Введите k:  "))
+        b = float(input("Введите b:  "))"""
+
+    if pygame.key.get_pressed()[pygame.K_SPACE]:
+        pygame.draw.line(gamedisplay, (0, 0, 0), (1200, 400), (1195, 405), 2)
+        pygame.draw.line(gamedisplay, (0, 0, 0), (600, 0), (600, 800), 2)
+        pygame.draw.line(gamedisplay, (0, 0, 0), (0, 400), (1200, 400), 2)
+        pygame.draw.line(gamedisplay, (0, 0, 0), (595, 5), (600, 0), 2)
+        pygame.draw.line(gamedisplay, (0, 0, 0), (600, 0), (605, 5), 2)
+        pygame.draw.line(gamedisplay, (0, 0, 0), (1195, 395), (1200, 400), 2)
+        pygame.draw.line(gamedisplay, (0, 0, 0), (1200, 400), (1195, 405), 2)
